967912af4cdcf89df665579708af27c51c808d9b
Ed Warnicke <eaw@cisco.com>
2015-04-13 22:09:48 -0700

    Create the tunnel port in OVSDB, and connect.
    This patch creates a tunnel port for ofoverlay, and
    optionally connects it to the controller (set by member
    in the class).
    
    Change-Id: Iac2f099cbe3521c8b9d53bf55752264543e4e22d
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

--- renderers/ofoverlay/src/main/java/org/opendaylight/groupbasedpolicy/renderer/ofoverlay/OFOverlayRenderer.java
+++ renderers/ofoverlay/src/main/java/org/opendaylight/groupbasedpolicy/renderer/ofoverlay/OFOverlayRenderer.java
@@ -18,6 +18,7 @@ import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
 import org.opendaylight.groupbasedpolicy.resolver.PolicyResolver;
+import org.opendaylight.groupbasedpolicy.renderer.ofoverlay.ovsdb.OfOverlayOvsdbBridgeDataChangeListener;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.groupbasedpolicy.ofoverlay.rev140528.OfOverlayConfig;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.yang.binding.DataObject;
@@ -44,6 +45,7 @@ public class OFOverlayRenderer implements AutoCloseable, DataChangeListener {
     private final SwitchManager switchManager;
     private final EndpointManager endpointManager;
     private final PolicyManager policyManager;
+    private final OfOverlayOvsdbBridgeDataChangeListener ovsdbListener;
 
     private final ScheduledExecutorService executor;
 
@@ -56,6 +58,7 @@ public class OFOverlayRenderer implements AutoCloseable, DataChangeListener {
     public OFOverlayRenderer(DataBroker dataProvider,
                              RpcProviderRegistry rpcRegistry) {
         super();
+        ovsdbListener = new OfOverlayOvsdbBridgeDataChangeListener(dataProvider);
         this.dataBroker = dataProvider;
 
         int numCPU = Runtime.getRuntime().availableProcessors();
@@ -95,6 +98,7 @@ public class OFOverlayRenderer implements AutoCloseable, DataChangeListener {
         if (policyResolver != null) policyResolver.close();
         if (switchManager != null) switchManager.close();
         if (endpointManager != null) endpointManager.close();
+        if (ovsdbListener != null) ovsdbListener.close();
     }
 
     // ******************
