427c53672030ba52262e8432aacd6e1414012290
Kinsey Nietzsche <knietzsc@cisco.com>
2015-08-07 11:42:57 +0200

    Bug 3702 - UT GBP - 13 - groupbasedpolicy-base
    PolicyResolverUtils.java (73.1%)
    PolicyResolutionException.java (100%)
    PolicyResolver.java (58.1%)
    EndpointConstraint.java (88%)
    PolicyValidator.java (95.1%)
    SimpleResult.java (100%)
    ValidationResult (85.1%)
    
    Change-Id: I63eb56c4b5e94366896c7f0b40a1c028dc181556
    Signed-off-by: Kinsey Nietzsche <knietzsc@cisco.com>

--- groupbasedpolicy/src/main/java/org/opendaylight/groupbasedpolicy/resolver/validator/PolicyValidator.java
+++ groupbasedpolicy/src/main/java/org/opendaylight/groupbasedpolicy/resolver/validator/PolicyValidator.java
@@ -47,7 +47,7 @@ public class PolicyValidator {
      * additional data required for individual validation process
      * @return Concrete {@link Validator} for given object
      */
-    @SuppressWarnings("unchecked")
+    @SuppressWarnings({"unchecked", "rawtypes"})
     static <T extends DataContainer> Validator<T> createValidator(T object, PolicyResolver policyResolver) {
         AbstractValidator validator = VALIDATORS.get(object.getImplementedInterface());
         if (validator == null) {
