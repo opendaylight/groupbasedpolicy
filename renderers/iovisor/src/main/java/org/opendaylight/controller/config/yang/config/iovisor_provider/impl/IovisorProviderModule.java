b6095e885945ac5242a5fb9acd19096dd11bea30
Keith Burns (alagalah) <alagalah@gmail.com>
2015-11-03 18:27:28 -0800

    New features
    - IOVisor renderer
    - OVSSFC renderer
    - Netconf renderer
    
    Change-Id: Ie061a9dd99bd4fa6c1c77399e9616c9f9bb3eff3
    Signed-off-by: Keith Burns (alagalah) <alagalah@gmail.com>

---
+++ renderers/iovisor/src/main/java/org/opendaylight/controller/config/yang/config/iovisor_provider/impl/IovisorProviderModule.java
@@ -0,0 +1,22 @@
+package org.opendaylight.controller.config.yang.config.iovisor_provider.impl;
+public class IovisorProviderModule extends org.opendaylight.controller.config.yang.config.iovisor_provider.impl.AbstractIovisorProviderModule {
+    public IovisorProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public IovisorProviderModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.controller.config.yang.config.iovisor_provider.impl.IovisorProviderModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        // TODO:implement
+        throw new java.lang.UnsupportedOperationException();
+    }
+
+}
