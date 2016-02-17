1f1ca90df0c08dbcfd2afbfcb095bf741f99cca8
Matej Perina <matej.perina@pantheon.sk>
2016-02-17 14:02:08 +0100

    Bug 3738: GBP coexistence fixes
    - chmod scripts, small script issues
    
    Change-Id: Ice6c53dbcf134fb35f68985c7a3902e263cddcff
    Signed-off-by: Keith Burns (alagalah) <alagalah@gmail.com>
    Signed-off-by: Matej Perina <matej.perina@pantheon.sk>

--- demos/gbp-devstack/devstack-scripts/sfc/sfc04.sh
+++ demos/gbp-devstack/devstack-scripts/sfc/sfc04.sh
@@ -7,9 +7,34 @@ fi
 echo "writing lock file /home/vagrant/sfc04.lock"
 touch /home/vagrant/sfc04.lock
 
-python /vagrant/devstack-scripts/sfc/chain.py $ODL client_sg web_sg mychain
+neutron security-group-rule-list
+
+echo 'Select a security-group-rule UUID from the above list to apply a chain to. This step can be repeated to add more security group rules to the chain.
+
+You can enter this either via the YangUI or sending:
+
+http://localhost:8181/restconf/operations/neutron-gbp-mapper:change-action-of-security-group-rules
+{
+    "input": {
+        "security-group-rule": [
+            {
+                "uuid": "<uuid of rule1>"
+            },
+            {
+                "uuid": "<uuid of rule2>"
+            }
+        ],
+        "action": {
+            "sfc-chain-name": "mychain"
+        }
+    }
+}'
+
+#python /vagrant/devstack-scripts/sfc/chain.py $ODL client_sg web_sg mychain
 
 echo "Chain action added to policy. 
+
+Enter route for SF on devstack-compute-1:
 
 kernelmods.sh
 "
