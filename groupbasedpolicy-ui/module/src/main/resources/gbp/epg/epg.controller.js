15dfd4ef00787729219a1d24ef867083b910ddfe
Ľubomír Balogh <lubomir.balogh@pantheon.local>
2016-05-09 10:55:42 +0200

    EPG controller and service
    Signed-off-by: Ľubomír Balogh <lubomir.balogh@pantheon.local>

--- groupbasedpolicy-ui/module/src/main/resources/gbp/epg/epg.controller.js
+++ groupbasedpolicy-ui/module/src/main/resources/gbp/epg/epg.controller.js
@@ -1,13 +1,21 @@
 define([
-
-], function () {
+    'app/gbp/epg/epg.service',
+    'app/gbp/epg/epg-list.service',
+], function() {
     'use strict';
 
     angular.module('app.gbp').controller('EpgController', EpgController);
 
-    EpgController.$inject = ['$scope'];
+    EpgController.$inject = ['$scope', 'EpgService', 'EpgListService'];
 
-    function EpgController($scope) {
+    function EpgController($scope, EpgService, EpgListService) {
+        $scope.epg = EpgService.createObject();
+        $scope.epg.get('epg_test', 'tenant_test');
+        console.log('Epg', $scope.epg);
 
+        $scope.epgs = EpgListService.createList();
+        $scope.epgs.get('config', 'tenant2');
+
+        console.log('Epgs', $scope.epgs);
     }
 });
