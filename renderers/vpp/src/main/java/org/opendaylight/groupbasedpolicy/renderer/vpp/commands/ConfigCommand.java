ec6f67745b9665e3e3e14a0985b5514dfbe9d69b
Michal Cmarada <mcmarada@cisco.com>
2016-07-15 08:47:11 +0200

    Tap Port implementation in VPP renderer
    Change-Id: I210368f121722d13703a6d0810f44ccab1e5ea02
    Signed-off-by: Michal Cmarada <mcmarada@cisco.com>

--- renderers/vpp/src/main/java/org/opendaylight/groupbasedpolicy/renderer/vpp/commands/ConfigCommand.java
+++ renderers/vpp/src/main/java/org/opendaylight/groupbasedpolicy/renderer/vpp/commands/ConfigCommand.java
@@ -9,14 +9,21 @@
 package org.opendaylight.groupbasedpolicy.renderer.vpp.commands;
 
 import org.opendaylight.controller.md.sal.binding.api.ReadWriteTransaction;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.InterfaceBuilder;
 
 public interface ConfigCommand {
 
     /**
-     * Execute command using a given data modification transaction
+     * Execute command using a given data modification transaction.
      *
      * @param readWriteTransaction Transaction for command execution
      */
     void execute(ReadWriteTransaction readWriteTransaction);
 
+    /**
+     * Creates Interface Builder with proper augmentations.
+     *
+     * @return InterfaceBuilder for this command with proper augmentations
+     */
+    InterfaceBuilder getInterfaceBuilder();
 }
---
+++ renderers/vpp/src/main/java/org/opendaylight/groupbasedpolicy/renderer/vpp/commands/TapPortCommand.java
@@ -0,0 +1,194 @@
+/*
+ * Copyright (c) 2016 Cisco Systems. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.groupbasedpolicy.renderer.vpp.commands;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+import org.opendaylight.groupbasedpolicy.renderer.vpp.util.General;
+import org.opendaylight.groupbasedpolicy.renderer.vpp.util.General.Operations;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.Interface;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.InterfaceBuilder;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.interfaces.rev140508.interfaces.InterfaceKey;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.PhysAddress;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.v3po.rev150105.Tap;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.v3po.rev150105.VppInterfaceAugmentation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.v3po.rev150105.VppInterfaceAugmentationBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.v3po.rev150105.interfaces._interface.L2Builder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.v3po.rev150105.interfaces._interface.TapBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.v3po.rev150105.l2.base.attributes.interconnection.BridgeBasedBuilder;
+
+public class TapPortCommand extends AbstractInterfaceCommand<TapPortCommand> {
+
+    private String tapName;
+    private PhysAddress physAddress;
+    private String bridgeDomain;
+    private Long deviceInstance;
+
+    private TapPortCommand(TapPortCommandBuilder builder) {
+        this.name = builder.getInterfaceName();
+        this.tapName = builder.getTapName();
+        this.operation = builder.getOperation();
+        this.physAddress = builder.getPhysAddress();
+        this.enabled = builder.isEnabled();
+        this.description = builder.getDescription();
+        this.bridgeDomain = builder.getBridgeDomain();
+        this.deviceInstance = builder.getDeviceInstance();
+    }
+
+    public static TapPortCommandBuilder builder() {
+        return new TapPortCommandBuilder();
+    }
+
+    public PhysAddress getPhysAddress() {
+        return physAddress;
+    }
+
+    public String getBridgeDomain() {
+        return bridgeDomain;
+    }
+
+    public Long getDeviceInstance() {
+        return deviceInstance;
+    }
+
+    public String getTapName() {
+        return tapName;
+    }
+
+    @Override
+    public InterfaceBuilder getInterfaceBuilder() {
+        InterfaceBuilder interfaceBuilder = new InterfaceBuilder().setKey(new InterfaceKey(name))
+            .setEnabled(enabled)
+            .setDescription(description)
+            .setType(Tap.class)
+            .setName(name)
+            .setLinkUpDownTrapEnable(Interface.LinkUpDownTrapEnable.Enabled);
+
+        // Create the Tap augmentation
+        VppInterfaceAugmentationBuilder vppAugmentationBuilder =
+                new VppInterfaceAugmentationBuilder().setTap(new TapBuilder().setMac(this.physAddress)
+                    .setTapName(this.tapName)
+                    .setMac(this.physAddress)
+                    .setDeviceInstance(this.deviceInstance)
+                    .build());
+
+        if (!Strings.isNullOrEmpty(bridgeDomain)) {
+            vppAugmentationBuilder.setL2(new L2Builder()
+                .setInterconnection(new BridgeBasedBuilder().setBridgeDomain(bridgeDomain).build()).build());
+        }
+
+        interfaceBuilder.addAugmentation(VppInterfaceAugmentation.class, vppAugmentationBuilder.build());
+        return interfaceBuilder;
+    }
+
+    @Override
+    public String toString() {
+        return "TapPortUserCommand [physAddress=" + physAddress + ", bridgeDomain=" + bridgeDomain + ", operations="
+                + operation + ", name=" + name + ", tapName=" + tapName + ", description=" + description + ", enabled="
+                + enabled + "]";
+    }
+
+    public static class TapPortCommandBuilder {
+
+        private String interfaceName;
+        private String tapName;
+        private Operations operation;
+        private PhysAddress physAddress;
+        private String bridgeDomain;
+        private String description;
+        private Long deviceInstance = null;
+        private boolean enabled = true;
+
+        public String getInterfaceName() {
+            return interfaceName;
+        }
+
+        public TapPortCommandBuilder setInterfaceName(String interfaceName) {
+            this.interfaceName = interfaceName;
+            return this;
+        }
+
+        public String getTapName() {
+            return tapName;
+        }
+
+        public TapPortCommandBuilder setTapName(String tapName) {
+            this.tapName = tapName;
+            return this;
+        }
+
+        public General.Operations getOperation() {
+            return operation;
+        }
+
+        public TapPortCommandBuilder setOperation(Operations operation) {
+            this.operation = operation;
+            return this;
+        }
+
+        public PhysAddress getPhysAddress() {
+            return physAddress;
+        }
+
+        public TapPortCommandBuilder setPhysAddress(PhysAddress physAddress) {
+            this.physAddress = physAddress;
+            return this;
+        }
+
+        public String getBridgeDomain() {
+            return bridgeDomain;
+        }
+
+        public TapPortCommandBuilder setBridgeDomain(String bridgeDomain) {
+            this.bridgeDomain = bridgeDomain;
+            return this;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public TapPortCommandBuilder setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Long getDeviceInstance() {
+            return deviceInstance;
+        }
+
+        public TapPortCommandBuilder setDeviceInstance(Long deviceInstance) {
+            this.deviceInstance = deviceInstance;
+            return this;
+        }
+
+        public boolean isEnabled() {
+            return enabled;
+        }
+
+        public TapPortCommandBuilder setEnabled(boolean enabled) {
+            this.enabled = enabled;
+            return this;
+        }
+
+        /**
+         * TapPortCommand build method.
+         *
+         * @return TapPortCommand
+         * @throws IllegalArgumentException if interfaceName, tapName or operation is null.
+         */
+        public TapPortCommand build() {
+            Preconditions.checkArgument(this.interfaceName != null);
+            Preconditions.checkArgument(this.tapName != null);
+            Preconditions.checkArgument(this.operation != null);
+
+            return new TapPortCommand(this);
+        }
+    }
+}
