152d62feaad519d59d544d7e53282bf6a5a9a4d3
Michal Rehak <mirehak@cisco.com>
2016-08-17 17:12:00 +0200

    BUG-6650: ep-ip/sgt, rename groupbasedpolicy-ise-adapter to sxp-ise-adapter
    Change-Id: I30845d92798bb37f485ea87bfd3cd516d657c295
    Signed-off-by: Michal Rehak <mirehak@cisco.com>

---
+++ sxp-integration/sxp-ise-adapter/src/main/java/org/opendaylight/groupbasedpolicy/sxp_ise_adapter/impl/GbpIseSgtHarvester.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.groupbasedpolicy.sxp_ise_adapter.impl;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import javax.annotation.Nonnull;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.groupbasedpolicy.gbp.sxp.ise.adapter.model.rev160630.gbp.sxp.ise.adapter.IseSourceConfig;
+
+/**
+ * Purpose: read sgts and naming from ISE via rest-API and have apropriate templates generated and stored
+ */
+public interface GbpIseSgtHarvester {
+
+    /**
+     * @param configuration user given
+     * @return amount of successfully written items
+     */
+    ListenableFuture<Integer> harvest(@Nonnull IseSourceConfig configuration);
+}
---
+++ sxp-integration/sxp-ise-adapter/src/main/java/org/opendaylight/groupbasedpolicy/sxp_ise_adapter/impl/util/RestClientFactory.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.groupbasedpolicy.sxp_ise_adapter.impl.util;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.client.urlconnection.HTTPSProperties;
+import java.security.GeneralSecurityException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import org.apache.commons.net.util.TrustManagerUtils;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.groupbasedpolicy.gbp.sxp.ise.adapter.model.rev160630.gbp.sxp.ise.adapter.ise.source.config.ConnectionConfig;
+
+/**
+ * Purpose: setup ise-ready jersey {@link Client}
+ */
+public class RestClientFactory {
+
+    private RestClientFactory() {
+        throw new IllegalAccessError("factory class - no instances supported");
+    }
+
+    /**
+     * @param connectionConfig config provided
+     * @return initiated jersey client - ready to talk to ise
+     *
+     * @throws GeneralSecurityException in case when insecure certificate hack fails
+     */
+    public static Client createIseClient(final ConnectionConfig connectionConfig) throws GeneralSecurityException {
+        final DefaultClientConfig clientConfig = new DefaultClientConfig();
+        clientConfig.getProperties()
+                .put(ClientConfig.PROPERTY_CONNECT_TIMEOUT, connectionConfig.getConnectionTimeout());
+        clientConfig.getProperties()
+                .put(ClientConfig.PROPERTY_READ_TIMEOUT, connectionConfig.getReadTimeout());
+
+        hackInsecureCertificate(clientConfig);
+        return Client.create(clientConfig);
+    }
+
+    private static void hackInsecureCertificate(final ClientConfig clientConfigArg)
+            throws NoSuchAlgorithmException, KeyManagementException {
+        final TrustManager[] trustAllCerts = new TrustManager[]{TrustManagerUtils.getAcceptAllTrustManager()};
+
+        SSLContext sslContext = SSLContext.getInstance("SSL");
+        sslContext.init(null, trustAllCerts, null);
+
+        clientConfigArg.getProperties().put(HTTPSProperties.PROPERTY_HTTPS_PROPERTIES, new HTTPSProperties(
+                (s, sslSession) -> true,
+                sslContext
+        ));
+    }
+}
