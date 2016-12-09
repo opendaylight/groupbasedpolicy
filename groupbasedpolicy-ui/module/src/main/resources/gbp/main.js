2694f288751db10f79ec4d76ff2c785f71cb2e34
Daniel Malachovsky <dmalacho@cisco.com>
2016-12-09 13:24:18 +0100

    Fix collision between VBD UI and GBP UI
    Change-Id: Icb9fdae85e8f45e0a85e354ae441cf5bcf43016b
    Signed-off-by: Daniel Malachovsky <dmalacho@cisco.com>

--- groupbasedpolicy-ui/module/src/main/resources/gbp/main.js
+++ groupbasedpolicy-ui/module/src/main/resources/gbp/main.js
@@ -3,7 +3,6 @@ require.config({
         'angular-material': 'app/gbp/vendor/angular-material/angular-material.min',
         'angular-animate': 'app/gbp/vendor/angular-animate/angular-animate.min',
         'angular-aria': 'app/gbp/vendor/angular-aria/angular-aria.min',
-        'lodash': 'app/gbp/vendor/lodash/dist/lodash.min',
         'angular-material-data-table': 'app/gbp/vendor/angular-material-data-table/dist/md-data-table.min',
         'angular-messages': 'app/gbp/vendor/angular-messages/angular-messages.min',
         'next-ui': 'app/gbp/vendor/NeXt/js/next.min',
